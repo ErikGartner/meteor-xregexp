3d389624abdaf9664383a25aa8662d83f2f8b53c
Erik <xxxsmoizxxx@gmail.com>
2015-11-03 14:34:37 +0100

    First commit.

---
+++ package.js
@@ -0,0 +1,13 @@
+Package.describe({
+  name: 'smoiz:xregexp',
+  version: '1.0.0',
+  summary: 'A wrapper for the extended regex library XRegExp.',
+  git: 'https://github.com/smoiz/meteor-xregexp',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.1');
+  api.addFiles('xregexp-all-min.js');
+  api.export('XRegExp');
+});
