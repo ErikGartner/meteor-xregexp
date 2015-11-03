05cab6a65f13150d5a81c6be350ce6f1c59ba7d8
Erik <xxxsmoizxxx@gmail.com>
2015-11-03 14:47:49 +0100

    fix: Improper exposure of package variable.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'smoiz:xregexp',
-  version: '1.0.0',
+  version: '1.0.1',
   summary: 'A wrapper for the extended regex library XRegExp.',
   git: 'https://github.com/smoiz/meteor-xregexp',
   documentation: 'README.md'
@@ -8,6 +8,6 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
-  api.addFiles('xregexp-all-min.js');
+  api.addFiles('xregexp-all.js');
   api.export('XRegExp');
 });
